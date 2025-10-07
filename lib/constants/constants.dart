c06662b4b29ff84cdd1146c416b19c33f5855f41
Saadkhanprogziel <progzieldeveloper02@gmail.com>
2025-10-07 10:37:50 +0500

    improvements

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,3 +3,4 @@ const baseSokcetUrl = 'http://159.65.161.106:5000';
 // const baseSokcetUrl = 'http://192.168.18.46:5000';
 // const baseUrl = 'http://192.168.18.46:5000';
 const apiUrl = "$baseUrl/api/";
+
